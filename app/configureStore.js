332d1e9c8ac3fce85b8f644207f761ef21ac3099
Michael Symmes <msymmes@gmail.com>
2018-09-05 20:53:31 -0400

    Added a middleware for saving applications to localState

--- app/configureStore.js
+++ app/configureStore.js
@@ -7,6 +7,7 @@ import { fromJS } from 'immutable';
 import { routerMiddleware } from 'react-router-redux';
 import createSagaMiddleware from 'redux-saga';
 import createReducer from './reducers';
+import { ADD_APPLICATION } from './containers/App/constants';
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -14,9 +15,28 @@ export default function configureStore(initialState = {}, history) {
   // Create the store with two middlewares
   // 1. sagaMiddleware: Makes redux-sagas work
   // 2. routerMiddleware: Syncs the location/URL path to the state
+  const createLogger = ({ getState }) => (next) =>
+    (action) => {
+      // const prevState = getState().toJS();
+      const returnValue = next(action);
+      const nextState = getState().toJS();
+      const target = nextState.global.applications;
+      // const actionType = String(action.type);
+      switch (action.type) {
+        case ADD_APPLICATION:
+          window.console.log(String(action.type));
+          window.console.log('Saving applications to local storage.', target);
+          window.localStorage.setItem('applications', JSON.stringify(target));
+          return returnValue;
+        default:
+          return returnValue;
+      }
+    };
+
   const middlewares = [
     sagaMiddleware,
     routerMiddleware(history),
+    createLogger
   ];
 
   const enhancers = [
