3dbef53f5e158383cee76676c1aff75a475fd12f
Michael Symmes <msymmes@gmail.com>
2018-09-08 15:18:55 -0400

    Edit handling and tests.

--- app/containers/ApplicationManager/ApplicationManager.js
+++ app/containers/ApplicationManager/ApplicationManager.js
@@ -11,19 +11,39 @@ export default class ApplicationManager extends React.PureComponent {
     this.state = {
       showApplicationForm: false,
       showUpdateForm: false,
+      editing: false,
       item: null
     };
     this.toggleShowApplicationForm = this.toggleShowApplicationForm.bind(this);
     this.startUpdateItem = this.startUpdateItem.bind(this);
     this.finishUpdateItem = this.finishUpdateItem.bind(this);
+    this.startEditItem = this.startEditItem.bind(this);
+    this.edit = this.edit.bind(this);
   }
 
   toggleShowApplicationForm() {
     this.setState((state) => ({ showApplicationForm: !state.showApplicationForm }));
   }
 
+  startEditItem(item) {
+    this.setState(() => ({ showApplicationForm: true, item, editing: true }));
+  }
+
   startUpdateItem(item) {
     this.setState(() => ({ showUpdateForm: true, item }));
+  }
+
+  edit(item) {
+    // replace current state head with new item state
+    const currentItemState = this.state.item.state;
+    const nextItemState = currentItemState.map((state, i) => ((i === 0) ? item.state[0] : state));
+    const nextItem = Object.assign({}, this.state.item, {
+      company: item.company,
+      contact: item.contact,
+      state: nextItemState
+    });
+    this.props.updateApplication(nextItem);
+    this.setState(() => ({ editing: false, item: null }));
   }
 
   finishUpdateItem(item = null) {
@@ -33,12 +53,21 @@ export default class ApplicationManager extends React.PureComponent {
   }
 
   render() {
+    let item;
+    if (this.state.item) {
+      item = {
+        company: this.state.item.company,
+        contact: this.state.item.contact,
+        notes: this.state.item.state[0].notes,
+        status: this.state.item.state[0].status
+      };
+    }
     return (
       <article className="application-manager">
         <div className="application-manager__header">
           <h1 className="title is-inline is-size-4">App-Trax</h1>
           <button
-            className="application-manager__button-create-new -button button is-info is-small is-inline is-pulled-right"
+            className="application-manager__button-create-new button is-info is-small is-inline is-pulled-right"
             onClick={this.toggleShowApplicationForm}
           >
             {(this.state.showApplicationForm) ? 'x' : '+'}
@@ -46,9 +75,9 @@ export default class ApplicationManager extends React.PureComponent {
         </div>
         {(this.state.showApplicationForm) ?
           <ApplicationForm
-            onSubmit={this.props.addApplication}
+            onSubmit={(this.state.editing) ? this.edit : this.props.addApplication}
             onCancel={this.toggleShowApplicationForm}
-            item={this.state.item}
+            {...item}
           /> :
           ''
         }
@@ -62,7 +91,8 @@ export default class ApplicationManager extends React.PureComponent {
         {(!this.state.showApplicationForm && this.props.applications && this.props.applications.length > 0) ?
           <ApplicationsList
             apps={this.props.applications}
-            updateItem={this.startUpdateItem}
+            update={this.startUpdateItem}
+            edit={this.startEditItem}
           /> :
           ''
         }
--- app/containers/ApplicationManager/tests/applicationManager.test.js
+++ app/containers/ApplicationManager/tests/applicationManager.test.js
@@ -7,11 +7,13 @@ describe('ApplicationManager', () => {
     const page = shallow(<ApplicationManager />);
     expect(page.find('.application-manager__button-create-new').length).toEqual(1);
   });
+
   it('should open a create form when creation button clicked', () => {
     const page = mount(<ApplicationManager />);
     page.find('.application-manager__button-create-new').simulate('click');
     expect(page.find('.application-form').length).toEqual(1);
   });
+
   it('should close a create form when already open and creation button clicked', () => {
     const page = mount(<ApplicationManager />);
     page.find('.application-manager__button-create-new').simulate('click');
@@ -19,10 +21,12 @@ describe('ApplicationManager', () => {
     page.find('.application-manager__button-create-new').simulate('click');
     expect(page.find('.application-form').length).toEqual(0);
   });
+
   it('should not render an applications list when no applications exist', () => {
     const page = mount(<ApplicationManager applications={[]} />);
     expect(page.find('applications-list').length).toEqual(0);
   });
+
   it('should render an applications list when applications exist', () => {
     const page = mount(<ApplicationManager
       applications={[
@@ -35,5 +39,48 @@ describe('ApplicationManager', () => {
       ]}
     />);
     expect(page.find('.applications-list').length).toEqual(1);
+  });
+  describe('Editing', () => {
+    describe('::startEditItem', () => {
+      it('should initiate an edit action', () => {
+        const item = {
+          company: 'Test',
+          contact: 'testee@testers.com',
+          state: [{ status: 'Considering', notes: '' }]
+        };
+        const wrapper = shallow(<ApplicationManager />);
+        wrapper.instance().startEditItem(item);
+        expect(wrapper.state('showApplicationForm')).toEqual(true);
+        expect(wrapper.state('editing')).toEqual(true);
+        expect(wrapper.state('item')).toEqual(item);
+      });
+    });
+    describe('::edit', () => {
+      it('should update an edited record, set editing to false, and item to null', () => {
+        const updateApplication = jest.fn();
+        const item = {
+          company: 'Test',
+          contact: 'testee@testers.com',
+          state: [{ status: 'Rejected', notes: 'bummer' }, { status: 'Applied', notes: '' }]
+        };
+        const editedRecord = {
+          company: 'Tested',
+          contact: 'testee@testers.com',
+          state: [{ status: 'Accepted', notes: 'awesome!' }]
+        };
+        const expectedRecord = {
+          company: 'Tested',
+          contact: 'testee@testers.com',
+          state: [{ status: 'Accepted', notes: 'awesome!' }, { status: 'Applied', notes: '' }]
+        };
+        const wrapper = shallow(<ApplicationManager updateApplication={updateApplication} />);
+        wrapper.instance().startEditItem(item);
+        wrapper.instance().edit(editedRecord);
+        expect(wrapper.state('editing')).toEqual(false);
+        expect(wrapper.state('item')).toEqual(null);
+        expect(updateApplication).toBeCalled();
+        expect(updateApplication).toBeCalledWith(expectedRecord);
+      });
+    });
   });
 });
