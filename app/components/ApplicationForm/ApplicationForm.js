a856339fee0c09200cd27cce2f7a8baebc33505b
Michael Symmes <msymmes@gmail.com>
2018-09-07 23:05:59 -0400

    added container

--- app/components/ApplicationForm/ApplicationForm.js
+++ app/components/ApplicationForm/ApplicationForm.js
@@ -52,111 +52,113 @@ export default class ApplicationForm extends React.PureComponent {
 
   render() {
     return (
-      <div className="application-form section">
-        <h1 className="subtitle has-text-centered">Create New</h1>
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label" htmlFor="company">Company</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control">
-                <input
-                  id="company"
-                  className={`input ${(this.state.companyHelpMsg) ? 'is-danger' : ''}`}
-                  type="text"
-                  value={this.state.company}
-                  placeholder="e.g. Apple inc"
-                  onChange={(e) => this.handleChangeField('company', e.target.value)}
-                />
-              </div>
-              {(this.state.companyHelpMsg) ?
-                <p className="application-form__company-help-msg help is-danger">
-                  Must not be empty
-                </p> : ''
-              }
+      <section className="application-form section">
+        <div className="container">
+          <h1 className="subtitle has-text-centered">Create New</h1>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label" htmlFor="company">Company</label>
             </div>
-          </div>
-        </div>
-
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label" htmlFor="contact">Contact</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control">
-                <input
-                  id="contact"
-                  className="input"
-                  type="text"
-                  value={this.state.contact}
-                  placeholder="e.g. Mark Jacobs <mark@apple.com>"
-                  onChange={(e) => this.handleChangeField('contact', e.target.value)}
-                />
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <input
+                    id="company"
+                    className={`input ${(this.state.companyHelpMsg) ? 'is-danger' : ''}`}
+                    type="text"
+                    value={this.state.company}
+                    placeholder="e.g. Apple inc"
+                    onChange={(e) => this.handleChangeField('company', e.target.value)}
+                  />
+                </div>
+                {(this.state.companyHelpMsg) ?
+                  <p className="application-form__company-help-msg help is-danger">
+                    Must not be empty
+                  </p> : ''
+                }
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label" htmlFor="notes">Notes</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control">
-                <textarea
-                  id="notes"
-                  className="textarea"
-                  value={this.state.notes}
-                  placeholder="e.g. Applied online at.... Application included technical questions and resume attachment."
-                  onChange={(e) => this.handleChangeField('notes', e.target.value)}
-                />
-              </div>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label" htmlFor="contact">Contact</label>
             </div>
-          </div>
-        </div>
-
-        <div className="field is-horizontal">
-          <div className="field-label is-normal">
-            <label className="label" htmlFor="status">Status</label>
-          </div>
-          <div className="field-body">
-            <div className="field">
-              <div className="control">
-                <div className="select is-fullwidth">
-                  <select
-                    value={this.state.status}
-                    onChange={(e) => this.handleChangeField('status', e.target.value)}
-                  >
-                    {statusOptions.map((o) => <option key={o}>{o}</option>)}
-                  </select>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <input
+                    id="contact"
+                    className="input"
+                    type="text"
+                    value={this.state.contact}
+                    placeholder="e.g. Mark Jacobs <mark@apple.com>"
+                    onChange={(e) => this.handleChangeField('contact', e.target.value)}
+                  />
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="field is-grouped is-grouped-centered">
-          <div className="control">
-            <button
-              className="application-form__submit-button button is-primary"
-              onClick={this.handleSubmit}
-            >
-              Create
-            </button>
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label" htmlFor="notes">Notes</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <textarea
+                    id="notes"
+                    className="textarea"
+                    value={this.state.notes}
+                    placeholder="e.g. Applied online at.... Application included technical questions and resume attachment."
+                    onChange={(e) => this.handleChangeField('notes', e.target.value)}
+                  />
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="control">
-            <button
-              className="application-form__cancel-button button is-text"
-              onClick={this.props.onCancel}
-            >
-              Cancel
-            </button>
+
+          <div className="field is-horizontal">
+            <div className="field-label is-normal">
+              <label className="label" htmlFor="status">Status</label>
+            </div>
+            <div className="field-body">
+              <div className="field">
+                <div className="control">
+                  <div className="select is-fullwidth">
+                    <select
+                      value={this.state.status}
+                      onChange={(e) => this.handleChangeField('status', e.target.value)}
+                    >
+                      {statusOptions.map((o) => <option key={o}>{o}</option>)}
+                    </select>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="field is-grouped is-grouped-centered">
+            <div className="control">
+              <button
+                className="application-form__submit-button button is-primary"
+                onClick={this.handleSubmit}
+              >
+                Create
+              </button>
+            </div>
+            <div className="control">
+              <button
+                className="application-form__cancel-button button is-text"
+                onClick={this.props.onCancel}
+              >
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     );
   }
 }
