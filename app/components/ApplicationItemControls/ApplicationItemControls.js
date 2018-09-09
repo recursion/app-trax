0370b6059eec4824d1bb8d36b533801a99af5971
Michael Symmes <msymmes@gmail.com>
2018-09-09 00:30:56 -0400

    decreased icon size.

--- app/components/ApplicationItemControls/ApplicationItemControls.js
+++ app/components/ApplicationItemControls/ApplicationItemControls.js
@@ -4,14 +4,14 @@ import PropTypes from 'prop-types';
 const ApplicationItemControls = (props) => (
   <span className="application-item__controls">
     <button onClick={() => props.edit(props.app)}>
-      <span className="icon is-size-6">
+      <span className="icon is-size-7">
         <i className="fas fa-edit"></i>
       </span>
     </button>
     {
       (props.itemState.length > 1) ?
         <button className="application-item__history-button">
-          <span className="icon is-size-6">
+          <span className="icon is-size-7">
             <i className="fas fa-history"></i>
           </span>
         </button> :
