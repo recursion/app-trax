64a55fdbe5d8831854f703bcae7806140a00df27
Michael Symmes <msymmes@gmail.com>
2018-09-11 13:48:07 -0400

    styling to improve scrolling on multiple displays.

--- app/components/Footer/Footer.js
+++ app/components/Footer/Footer.js
@@ -3,8 +3,6 @@ import './style.scss';
 
 const Footer = () => (
   <footer>
-    <section>This project is licensed under the MIT license.</section>
-    <section>Made with <span role="img" aria-label="heart-emoji">❤️</span> by Michael Symmes</section>
   </footer>
 );
 
