09925d804dcaf36b1cdfe6c1387158b6fb29dae7
Michael Symmes <msymmes@gmail.com>
2018-09-12 13:06:24 -0400

    fixed tests to use a setup.

--- app/containers/ApplicationForm/tests/index.test.js
+++ app/containers/ApplicationForm/tests/index.test.js
@@ -1,135 +1,93 @@
 import React from 'react';
-import { MemoryRouter } from 'react-router';
 import MockRouter from 'react-mock-router';
 import { mount } from 'enzyme';
 import ApplicationForm from '../ApplicationForm';
 
+const setup = (propOverrides, params) => {
+  const props = Object.assign({
+    applications: [
+      {
+        company: 'Test',
+        contact: 'test',
+        state: [{ status: 'Testing', notes: '', updated: Date.now }, { status: 'Testing2', notes: '2forYou', updated: Date.now }],
+        createdAt: Date.now(),
+        id: 'lkjasdlf9109123lk109d'
+      },
+      {
+        company: 'Test2',
+        contact: 'test@test.com',
+        state: [{ status: 'Applied', notes: 'Stuff', updated: Date.now() }],
+        createdAt: Date.now(),
+        id: 'lkj2s8lf9109123lk109d-alsd2333'
+      }
+    ],
+    addApplication: jest.fn(),
+    deleteApplication: jest.fn(),
+    updateApplication: jest.fn()
+  }, propOverrides);
+
+  const wrapper = mount(
+    <MockRouter params={params} >
+      <ApplicationForm {...props} />
+    </MockRouter>
+  );
+
+  return {
+    props,
+    wrapper
+  };
+};
+
 describe('ApplicationForm', () => {
   it('renders an ApplicationForm', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <ApplicationForm
-          addApplication={() => {}}
-          deleteApplication={() => {}}
-          updateApplication={() => {}}
-        />
-      </MemoryRouter>
-    );
+    const { wrapper } = setup();
     expect(wrapper.find('.application-form').length).toEqual(1);
   });
 
   it('renders a Create New title when no application props were passed in', () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <ApplicationForm
-          addApplication={() => {}}
-          deleteApplication={() => {}}
-          updateApplication={() => {}}
-        />
-      </MemoryRouter>
-    );
+    const { wrapper } = setup();
     expect(wrapper.find('.application-form__title').text()).toEqual('Create New');
   });
 
   it('renders an Edit title when given a valid application id', () => {
-    const id = Date.now();
-    const mockApps = [
-      {
-        company: 'Test',
-        createdAt: id
-      }
-    ];
-    const wrapper = mount(
-      <MockRouter params={{ id }}>
-        <ApplicationForm
-          addApplication={() => {}}
-          deleteApplication={() => {}}
-          updateApplication={() => {}}
-          applications={mockApps}
-        />
-      </MockRouter>
-    );
+    const { wrapper } = setup({}, { id: 'lkjasdlf9109123lk109d' });
     expect(wrapper.find('.application-form__title').text()).toEqual('Edit');
   });
+
   describe('Create New', () => {
     it('it will not submit without a company name', () => {
-      const addMock = jest.fn();
-      const wrapper = mount(
-        <MemoryRouter>
-          <ApplicationForm
-            addApplication={addMock}
-            deleteApplication={() => {}}
-            updateApplication={() => {}}
-          />
-        </MemoryRouter>
-      );
+      const { props: { addApplication }, wrapper } = setup();
       wrapper.find('.application-form__submit-button').simulate('click');
-      expect(addMock).not.toBeCalled();
+      expect(addApplication).not.toBeCalled();
     });
 
     it('adds a createdAt property to the object', () => {
-      const addMock = jest.fn();
-      const wrapper = mount(
-        <MemoryRouter>
-          <ApplicationForm
-            addApplication={addMock}
-            deleteApplication={() => {}}
-            updateApplication={() => {}}
-          />
-        </MemoryRouter>
-      );
+      const { props: { addApplication }, wrapper } = setup();
       wrapper.find('#company').simulate('change', { target: { value: 'Test Company' } });
       wrapper.find('#contact').simulate('change', { target: { value: 'me@you.com' } });
       wrapper.find('.application-form__submit-button').simulate('click');
-      expect('createdAt' in addMock.mock.calls[0][0]).toEqual(true);
+      expect('createdAt' in addApplication.mock.calls[0][0]).toEqual(true);
     });
 
     it('renders a help message when submit is clicked with an empty company name', () => {
-      const addMock = jest.fn();
-      const wrapper = mount(
-        <MemoryRouter>
-          <ApplicationForm
-            addApplication={addMock}
-            deleteApplication={() => {}}
-            updateApplication={() => {}}
-          />
-        </MemoryRouter>
-      );
+      const { wrapper } = setup();
       wrapper.find('.application-form__submit-button').simulate('click');
       expect(wrapper.find('.application-form__company-help-msg').length).toEqual(1);
     });
 
     it('calls the addApplication function when the submit button is clicked with valid form data', () => {
-      const addMock = jest.fn();
-      const wrapper = mount(
-        <MemoryRouter>
-          <ApplicationForm
-            addApplication={addMock}
-            deleteApplication={() => {}}
-            updateApplication={() => {}}
-          />
-        </MemoryRouter>
-      );
+      const { props: { addApplication }, wrapper } = setup();
       const input = wrapper.find('#company');
       input.simulate('change', { target: { value: 'Test Company' } });
       wrapper.find('.application-form__submit-button').simulate('click');
-      expect(addMock).toBeCalled();
+      expect(addApplication).toBeCalled();
     });
 
     it('passes the form data to the addApplication function when submit is clicked', () => {
-      const addMock = jest.fn();
-      const wrapper = mount(
-        <MemoryRouter>
-          <ApplicationForm
-            addApplication={addMock}
-            deleteApplication={() => {}}
-            updateApplication={() => {}}
-          />
-        </MemoryRouter>
-      );
       const notes = 'Wootsy doots and wutsy wuts';
       const company = 'Test Company';
 
+      const { props: { addApplication }, wrapper } = setup();
       const companyNameInput = wrapper.find('#company');
       companyNameInput.simulate('change', { target: { value: company } });
 
@@ -138,8 +96,8 @@ describe('ApplicationForm', () => {
 
       wrapper.find('.application-form__submit-button').simulate('click');
 
-      const submittedApplication = addMock.mock.calls[0][0];
-      expect(addMock).toBeCalled();
+      const submittedApplication = addApplication.mock.calls[0][0];
+      expect(addApplication).toBeCalled();
       expect('company' in submittedApplication);
       expect('state' in submittedApplication);
       expect(submittedApplication.company).toEqual(company);
@@ -149,19 +107,10 @@ describe('ApplicationForm', () => {
 
     describe('Returned Application Data', () => {
       it('creates a state property that is an array of objects with notes, status, and updated props', () => {
-        const addMock = jest.fn();
-        const wrapper = mount(
-          <MemoryRouter>
-            <ApplicationForm
-              addApplication={addMock}
-              deleteApplication={() => {}}
-              updateApplication={() => {}}
-            />
-          </MemoryRouter>
-        );
         const notes = 'Wootsy doots and wutsy wuts';
         const company = 'Test Company';
 
+        const { props: { addApplication }, wrapper } = setup();
         const companyNameInput = wrapper.find('#company');
         companyNameInput.simulate('change', { target: { value: company } });
 
@@ -170,7 +119,7 @@ describe('ApplicationForm', () => {
 
         wrapper.find('.application-form__submit-button').simulate('click');
 
-        const submittedApplication = addMock.mock.calls[0][0];
+        const submittedApplication = addApplication.mock.calls[0][0];
 
         expect('state' in submittedApplication);
         expect(Array.isArray(submittedApplication.state)).toEqual(true);
@@ -182,88 +131,19 @@ describe('ApplicationForm', () => {
 
   describe('Edit existing', () => {
     it('renders a delete button if an application was passed in', () => {
-      const id = Date.now();
-      const mockApps = [{
-        company: 'Test',
-        contact: 'Tester@testing.com',
-        createdAt: id,
-        state: [
-          {
-            status: 'Considering',
-            notes: 'Hopeful on this one!',
-            updated: id
-          }
-        ]
-      }];
-      const wrapper = mount(
-        <MockRouter params={{ id }}>
-          <ApplicationForm
-            addApplication={() => {}}
-            deleteApplication={() => {}}
-            updateApplication={() => {}}
-            applications={mockApps}
-          />
-        </MockRouter>
-      );
+      const { wrapper } = setup({}, { id: 'lkjasdlf9109123lk109d' });
       expect(wrapper.find('.application-form__delete-button').length).toEqual(1);
     });
 
     it('calls the onDelete function when the delete button is clicked', () => {
-      const deleteApplication = jest.fn();
-      const id = Date.now();
-      const mockApps = [{
-        company: 'Test',
-        contact: 'Tester@testing.com',
-        createdAt: id,
-        state: [
-          {
-            status: 'Considering',
-            notes: 'Hopeful on this one!',
-            updated: id
-          }
-        ]
-      }];
-      const wrapper = mount(
-        <MockRouter params={{ id }}>
-          <ApplicationForm
-            addApplication={() => {}}
-            deleteApplication={deleteApplication}
-            updateApplication={() => {}}
-            applications={mockApps}
-          />
-        </MockRouter>
-      );
-
+      const { props: { deleteApplication }, wrapper } = setup({}, { id: 'lkjasdlf9109123lk109d' });
       wrapper.find('.application-form__delete-button').simulate('click');
       expect(deleteApplication).toBeCalled();
     });
 
     it('correctly renders existing company name, contact data, notes, and status when passed in', () => {
-      const deleteApplication = jest.fn();
-      const id = Date.now();
-      const mockApps = [{
-        company: 'Test',
-        contact: 'Tester@testing.com',
-        createdAt: id,
-        state: [
-          {
-            status: 'Considering',
-            notes: 'Hopeful on this one!',
-            updated: id
-          }
-        ]
-      }];
-      const item = mockApps[0];
-      const wrapper = mount(
-        <MockRouter params={{ id }}>
-          <ApplicationForm
-            addApplication={() => {}}
-            deleteApplication={deleteApplication}
-            updateApplication={() => {}}
-            applications={mockApps}
-          />
-        </MockRouter>
-      );
+      const { props: { applications }, wrapper } = setup({}, { id: 'lkjasdlf9109123lk109d' });
+      const item = applications[0];
       expect(wrapper.find('#company').props().value).toEqual(item.company);
       expect(wrapper.find('#contact').props().value).toEqual(item.contact);
       expect(wrapper.find('#status').props().value).toEqual(item.state[0].status);
@@ -271,71 +151,15 @@ describe('ApplicationForm', () => {
     });
 
     it('does not overwrite an existing state array when updating the current state', () => {
-      const deleteApplication = jest.fn();
-      const updateMock = jest.fn();
-      const id = Date.now();
-      const mockApps = [{
-        company: 'Test',
-        contact: 'Tester@testing.com',
-        createdAt: id,
-        state: [
-          {
-            status: 'Considering',
-            notes: 'Hopeful on this one!',
-            updated: Date.parse('Dec 1, 2000')
-          },
-          {
-            status: 'Considering',
-            notes: 'Hopeful on this one!',
-            updated: Date.parse('Dec 25, 1998')
-          }
-        ]
-      }];
-      const wrapper = mount(
-        <MockRouter params={{ id }}>
-          <ApplicationForm
-            addApplication={() => {}}
-            deleteApplication={deleteApplication}
-            updateApplication={updateMock}
-            applications={mockApps}
-          />
-        </MockRouter>
-      );
+      const { props: { updateApplication }, wrapper } = setup({}, { id: 'lkjasdlf9109123lk109d' });
       wrapper.find('.application-form__submit-button').simulate('click');
-      expect(updateMock).toBeCalled();
-      expect(updateMock.mock.calls[0][0].state.length).toEqual(2);
+      expect(updateApplication).toBeCalled();
+      expect(updateApplication.mock.calls[0][0].state.length).toEqual(2);
     });
 
     it('updates all changed fields successfully on edit', () => {
-      const updateApplication = jest.fn();
-      const id = Date.now();
-      const mockApps = [{
-        company: 'Test',
-        contact: 'Tester@testing.com',
-        createdAt: id,
-        state: [
-          {
-            status: 'Applied',
-            notes: 'Lets go for it!',
-            updated: Date.parse('Dec 25, 1995')
-          },
-          {
-            status: 'Considering',
-            notes: 'Hopeful on this one!',
-            updated: Date.parse('Dec 12, 1990')
-          }
-        ]
-      }];
-      const wrapper = mount(
-        <MockRouter params={{ id }}>
-          <ApplicationForm
-            addApplication={() => {}}
-            deleteApplication={() => {}}
-            updateApplication={updateApplication}
-            applications={mockApps}
-          />
-        </MockRouter>
-      );
+      const { props: { updateApplication }, wrapper } = setup({}, { id: 'lkjasdlf9109123lk109d' });
+
       wrapper.find('#company').simulate('change', { target: { value: 'FU' } });
       wrapper.find('#contact').simulate('change', { target: { value: 'me@you.com' } });
       wrapper.find('#status').simulate('change', { target: { value: 'Offer Accepted' } });
