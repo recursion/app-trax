bec86b49608b93ef2432f515b8b38e01419cbe3c
Michael Symmes <msymmes@gmail.com>
2018-09-12 13:18:02 -0400

    updated test to use common setup

--- app/components/ApplicationUpdate/tests/index.test.js
+++ app/components/ApplicationUpdate/tests/index.test.js
@@ -3,45 +3,42 @@ import { mount } from 'enzyme';
 import uuidv4 from 'uuid/v4';
 import ApplicationUpdate from '../ApplicationUpdate';
 
-describe('ApplicationUpdate', () => {
-  it('renders the current status', () => {
-    const item = {
-      name: 'Test',
-      contact: 'testers',
-      state: [{ notes: '', status: 'Considering' }],
+const setup = (propOverrides) => {
+  const props = Object.assign({
+    item: {
+      company: 'Test2',
+      contact: 'test@test.com',
+      state: [{ status: 'Considering', notes: 'Stuff' }],
       createdAt: Date.now(),
       id: uuidv4()
-    };
-    const wrapper = mount(<ApplicationUpdate item={item} />);
+    },
+    update: jest.fn()
+  }, propOverrides);
+
+  const wrapper = mount(<ApplicationUpdate {...props} />);
+
+  return {
+    props,
+    wrapper
+  };
+};
+
+describe('ApplicationUpdate', () => {
+  it('renders the current status', () => {
+    const { wrapper } = setup();
     const test = wrapper.find('#status').props().value;
     expect(test).toEqual('Considering');
   });
 
   it('calls update with no args when cancel is clicked', () => {
-    const item = {
-      name: 'Test',
-      contact: 'testers',
-      state: [{ notes: '', status: 'Applied' }],
-      createdAt: Date.now(),
-      id: uuidv4()
-    };
-    const update = jest.fn();
-    const wrapper = mount(<ApplicationUpdate item={item} update={update} />);
+    const { wrapper, props: { update } } = setup();
     wrapper.find('.application-form__cancel-button').simulate('click');
     expect(update).toBeCalled();
     expect(update).toBeCalledWith();
   });
 
   it('calls update with an item when submit is clicked', () => {
-    const item = {
-      name: 'Test',
-      contact: 'testers',
-      state: [{ notes: '', status: 'Applied' }],
-      createdAt: Date.now(),
-      id: uuidv4()
-    };
-    const update = jest.fn();
-    const wrapper = mount(<ApplicationUpdate item={item} update={update} />);
+    const { wrapper, props: { update } } = setup();
     const input = wrapper.find('#status');
     input.simulate('change', { target: { value: 'Rejected' } });
     expect(wrapper.find('#status').props().value).toEqual('Rejected');
@@ -51,14 +48,7 @@ describe('ApplicationUpdate', () => {
   });
 
   it('adds an updatedAt property to the status object', () => {
-    const item = {
-      name: 'Test',
-      contact: 'testers',
-      state: [{ notes: '', status: 'Applied', updated: Date.now() }],
-      createdAt: Date.now()
-    };
-    const update = jest.fn();
-    const wrapper = mount(<ApplicationUpdate item={item} update={update} />);
+    const { wrapper, props: { update } } = setup();
     const input = wrapper.find('#status');
     input.simulate('change', { target: { value: 'Rejected' } });
     expect(wrapper.find('#status').props().value).toEqual('Rejected');
