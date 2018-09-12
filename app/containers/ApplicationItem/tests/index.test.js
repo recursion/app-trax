42d60e426603e8c5885d07a9a39da04bfb6cfc0e
Michael Symmes <msymmes@gmail.com>
2018-09-12 12:35:16 -0400

    Added setup to clean tests up.

--- app/containers/ApplicationItem/tests/index.test.js
+++ app/containers/ApplicationItem/tests/index.test.js
@@ -3,72 +3,60 @@ import { MemoryRouter } from 'react-router';
 import { shallow, mount } from 'enzyme';
 import ApplicationItem from '../ApplicationItem';
 
+const setup = (propOverrides) => {
+  const props = Object.assign({
+    app: {
+      company: 'Test2',
+      contact: 'test@test.com',
+      state: [{ status: 'Applied', notes: 'Stuff' }],
+      createdAt: Date.now(),
+      id: 'lkj2s8lf9109123lk109d-alsd2333'
+    },
+    update: jest.fn()
+  }, propOverrides);
+
+  const wrapper = shallow(<ApplicationItem {...props} />);
+
+  return {
+    props,
+    wrapper
+  };
+};
+
 describe('ApplicationItem', () => {
   it('renders an ApplicationItem', () => {
-    const update = () => {};
-    const app = {
-      company: 'test',
-      contact: 'test',
-      state: [{ status: '', notes: '' }],
-      createdAt: Date.now()
-    };
-    const wrapped = shallow(<ApplicationItem update={update} app={app} />);
-    expect(wrapped.find('.application-item').length).toEqual(1);
+    const { wrapper } = setup();
+    expect(wrapper.find('.application-item').length).toEqual(1);
   });
 
   it('shows no details by default', () => {
-    const update = jest.fn();
-    const app = {
-      company: 'test',
-      contact: 'test',
-      state: [{ status: '', notes: '' }],
-      createdAt: Date.now()
-    };
-    const wrapped = shallow(<ApplicationItem update={update} app={app} />);
-    expect(wrapped.find('.application-details').length).toEqual(0);
+    const { wrapper } = setup();
+    expect(wrapper.find('.application-details').length).toEqual(0);
   });
 
   it('renders an expand button', () => {
-    const update = () => {};
-    const app = {
-      company: 'test',
-      contact: 'test',
-      state: [{ status: '', notes: '' }],
-      createdAt: Date.now()
-    };
-    const wrapped = mount(<ApplicationItem update={update} app={app} />);
-    expect(wrapped.find('.application-item__expand-button').length).toEqual(1);
+    const { props } = setup();
+    const wrapper = mount(<ApplicationItem {...props} />);
+    expect(wrapper.find('.application-item__expand-button').length).toEqual(1);
   });
 
   it('expands the view when the expand button is clicked', () => {
-    const update = () => {};
-    const app = {
-      company: 'test',
-      contact: 'test',
-      state: [{ status: '', notes: '' }],
-      createdAt: Date.now()
-    };
-    const wrapped = mount(
+    const { props } = setup();
+    const wrapper = mount(
       <MemoryRouter>
-        <ApplicationItem update={update} app={app} />
+        <ApplicationItem {...props} />
       </MemoryRouter>
     );
-    expect(wrapped.find('.application-details').length).toEqual(0);
-    wrapped.find('.application-item__expand-button').simulate('click');
-    expect(wrapped.find('.application-details').length).toEqual(1);
+    expect(wrapper.find('.application-details').length).toEqual(0);
+    wrapper.find('.application-item__expand-button').simulate('click');
+    expect(wrapper.find('.application-details').length).toEqual(1);
   });
 
   it('minimizes the view when the collapse button is clicked', () => {
-    const update = () => {};
-    const app = {
-      company: 'test',
-      contact: 'test',
-      state: [{ status: '', notes: '' }],
-      createdAt: Date.now()
-    };
+    const { props } = setup();
     const wrapped = mount(
       <MemoryRouter>
-        <ApplicationItem update={update} app={app} />
+        <ApplicationItem {...props} />
       </MemoryRouter>
     );
     expect(wrapped.find('.application-details').length).toEqual(0);
