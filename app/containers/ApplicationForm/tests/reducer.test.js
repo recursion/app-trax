a9a210818d05644bb297b525397ee535f096c23e
Michael Symmes <msymmes@gmail.com>
2018-09-17 13:19:23 -0400

    Added test for SUBMIT_EDIT reducer

--- app/containers/ApplicationForm/tests/reducer.test.js
+++ app/containers/ApplicationForm/tests/reducer.test.js
@@ -1,6 +1,6 @@
 import { fromJS } from 'immutable';
 import reducer from '../reducer';
-import { SUBMIT_NEW } from '../constants';
+import { SUBMIT_NEW, SUBMIT_EDIT } from '../constants';
 
 
 describe('ApplicationForm Reducer', () => {
@@ -28,5 +28,38 @@ describe('ApplicationForm Reducer', () => {
     const result = reducer(initialState, { type: SUBMIT_NEW, application }).toJS();
     expect(result.company).toEqual('Test');
     expect(result.state.length).toEqual(1);
+  });
+  it('handles the SUBMIT_EDIT action properly', () => {
+    const application = {
+      company: 'Test',
+      contact: 'me@you.com',
+      createdAt: Date.now(),
+      id: 'aksuh3298af',
+      state: [
+        {
+          status: 'Recieved Reply',
+          notes: 'blah blah blah',
+          updated: Date.now()
+        },
+        {
+          status: 'Applied',
+          notes: 'blah blah blah',
+          updated: Date.now()
+        }
+      ]
+    };
+
+    const initialState = fromJS({
+      company: '',
+      contact: '',
+      createdAt: '',
+      state: [],
+      id: 0
+    });
+
+    const result = reducer(initialState, { type: SUBMIT_EDIT, application }).toJS();
+    expect(result.company).toEqual('Test');
+    expect(result.id).toEqual(application.id);
+    expect(result.state.length).toEqual(2);
   });
 });
