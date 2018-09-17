237612a3a03535f6426bf997043c5791fe5408bd
Michael Symmes <msymmes@gmail.com>
2018-09-17 17:16:21 -0400

    fixed formatting.

--- app/localStorage.js
+++ app/localStorage.js
@@ -1,22 +1,21 @@
 import { ADD_APPLICATION, UPDATE_APPLICATION, DELETE_APPLICATION } from './containers/App/constants';
 
-export const saveToLocalStorageMiddleWare = ({ getState }) => (next) =>
-  (action) => {
-    // const prevState = getState().toJS();
-    const returnValue = next(action);
-    const nextState = getState().toJS();
-    const target = nextState.global.applications;
-    // const actionType = String(action.type);
-    switch (action.type) {
-      case ADD_APPLICATION:
-      case DELETE_APPLICATION:
-      case UPDATE_APPLICATION:
-        window.localStorage.setItem('applications', JSON.stringify(target));
-        return returnValue;
-      default:
-        return returnValue;
-    }
-  };
+export const saveToLocalStorageMiddleWare = ({ getState }) => (next) => (action) => {
+  // const prevState = getState().toJS();
+  const returnValue = next(action);
+  const nextState = getState().toJS();
+  const target = nextState.global.applications;
+  // const actionType = String(action.type);
+  switch (action.type) {
+    case ADD_APPLICATION:
+    case DELETE_APPLICATION:
+    case UPDATE_APPLICATION:
+      window.localStorage.setItem('applications', JSON.stringify(target));
+      return returnValue;
+    default:
+      return returnValue;
+  }
+};
 
 // load applications state from localStorage
 export const loadState = () => {
