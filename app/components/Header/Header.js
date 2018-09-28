7f35b0909eebfc8b52a428ac5b1aa91157e74b9a
Michael Symmes <msymmes@gmail.com>
2018-09-27 23:43:35 -0400

    setup initial tailwind classes

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,42 +3,30 @@ import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './style.scss';
 
-class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
+export default class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <div className="header">
-        <div className="container">
-          <h1 className="title is-inline is-size-4 has-text-white">App-Trax</h1>
-          {(this.props.location.pathname === '/') ?
-            <Link className="create-new-button router-link is-pulled-right button is-small is-info" to="/applications/create">
-              <span className="icon">
-                <i className="fas fa-plus"></i>
-              </span>
-            </Link> : ''
-          }
-          {(this.props.location.pathname === '/applications/create' ||
+      <div className="flex items-center justify-between bg-black text-white p-1 mb-2">
+        <h1 className="">App-Trax</h1>
+        {(this.props.location.pathname === '/') &&
+          <Link className="header-btn" to="/applications/create">
+            <span className="icon">
+              <i className="fas fa-plus"></i>
+            </span>
+          </Link>
+        }
+        {(this.props.location.pathname === '/applications/create' ||
           this.props.location.pathname.includes('/applications/edit')
-          ) ?
-            <Link
-              className="router-link is-pulled-right button is-small is-danger"
-              to="/"
-            >
-              <span className="icon">
-                <i className="fas fa-times"></i>
-              </span>
-            </Link> : ''
-          }
-          {(this.props.location.pathname.includes('/applications/history')) ?
-            <Link
-              className="router-link is-pulled-right button is-small is-info"
-              to="/"
-            >
-              <span className="icon">
-                <i className="fas fa-backspace"></i>
-              </span>
-            </Link> : ''
-          }
-        </div>
+        ) &&
+          <Link
+            className="header-btn"
+            to="/"
+          >
+            <span className="icon">
+              <i className="fas fa-times"></i>
+            </span>
+          </Link>
+        }
       </div>
     );
   }
@@ -46,13 +34,4 @@ class Header extends React.Component { // eslint-disable-line react/prefer-state
 Header.propTypes = {
   location: PropTypes.object,
 };
-/*
-<Link className="router-link" to="/">
-  Home
-</Link>
-<Link className="router-link" to="/features">
-  Features
-</Link>
-*/
-export default Header;
 
